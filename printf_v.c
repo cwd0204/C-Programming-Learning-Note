5f4e42a5ab046c43c60c8958080d061d94777abc
cwd0204 <cwd0204@126.com>
2021-01-29 15:30:42 +0800

    d
    d

---
+++ printf_v.c
@@ -0,0 +1,13 @@
+int main()
+{
+    /*comment here */
+    const int num = 6;
+    printf('%d', num);
+    printf('%d', pow(4,3));
+    printf('%f',8.9 * 4.5);
+    num = 8;
+    
+    printf('%d',num);
+    
+    return 0;
+}
